709a914cc8a34b9909eacd5411dd6a6ecc9774d0
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2023-11-29 10:05:41 +0100

    fisrt commit ng app create + json data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
