dca72d0553e1619b3fc6f2076c5b96e84e9c2d21
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2023-11-29 15:07:35 +0100

    correction de bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { JobListComponent } from './job-list/job-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { JobListComponent } from './job-list/job-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
